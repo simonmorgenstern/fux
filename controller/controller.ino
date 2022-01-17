1b2e7ea819592c289419fb287ed2b1bae642f07b
Simon Morgenstern <simonmorgenstern2001@gmail.com>
2022-01-17 21:46:02 +0100

    feat(controller): setup bluetooth connection

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,17 +1,25 @@
 #include <Adafruit_NeoPixel.h>
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
+#define RX 10
+#define TX 11
 #define PIN 5
 #define NUM_PIXELS 268
 
+SoftwareSerial mySerial(RX, TX);
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, PIN, NEO_GRB + NEO_KHZ800);
+int currentLED = 0;
+String btData; 
 
 void setup() {
     strip.begin();
     strip.setBrightness(50);
+    strip.setPixelColor(currentLED, 255, 255, 255);
     strip.show(); // Initialize all pixels to 'off'
+    mySerial.begin(9600);
 }
 
 void loop() {
-
+    
 }
