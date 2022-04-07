f439bb9d0bcd166be102e742950d98dd7a0810b6
Simon Morgenstern <inf2012@lehre.dhbw-stuttgart.de>
2022-04-07 16:21:46 +0200

    feat: initialize app project

---
+++ iFux/iFux/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  iFux
+//
+//  Created by Simon Morgenstern on 28.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
