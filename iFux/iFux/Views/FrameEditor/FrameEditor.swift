9311040dabc0aadcf8705c71298de8cc511737d3
Simon Morgenstern <inf2012@lehre.dhbw-stuttgart.de>
2022-04-07 16:21:46 +0200

    feat: initialize app project

---
+++ iFux/iFux/Views/FrameEditor/FrameEditor.swift
@@ -0,0 +1,20 @@
+//
+//  FrameEditor.swift
+//  iFux
+//
+//  Created by Simon Morgenstern on 07.04.22.
+//
+
+import SwiftUI
+
+struct FrameEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameEditor()
+    }
+}
---
+++ iFux/iFux/iFuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFuxApp.swift
+//  iFux
+//
+//  Created by Simon Morgenstern on 28.03.22.
+//
+
+import SwiftUI
+
+@main
+struct iFuxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
