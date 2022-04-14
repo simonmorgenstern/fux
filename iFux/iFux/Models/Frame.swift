89a7a93598a2a38d80dba5e41473c9a23acb586c
Simon Morgenstern <inf2012@lehre.dhbw-stuttgart.de>
2022-04-14 10:17:30 +0200

    fix: create colors with double instead of 0-255

--- iFux/iFux/Models/Frame.swift
+++ iFux/iFux/Models/Frame.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct Frame {
     var id = UUID().uuidString
-    var pixelColor = [Color](repeatElement(Color(red: 255, green: 255, blue: 255, opacity: 1.0), count: 268))
+    var pixelColor = [Color](repeatElement(Color(red: 1.0, green: 1.0, blue: 1.0, opacity: 1.0), count: 268))
     var currentColor: Color
     var applePencilModus: Bool
     var brightness: Double
--- iFux/iFux/Views/FrameEditor.swift
+++ iFux/iFux/Views/FrameEditor.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct FrameEditor: View {
-    @State var frame = Frame(currentColor: Color(red: 255, green: 255, blue: 0, opacity: 1.0), applePencilModus: false, brightness: 25)
+    @State var frame = Frame(currentColor: Color(red: 1.0, green: 1.0, blue: 0, opacity: 1.0), applePencilModus: false, brightness: 25)
     
     var body: some View {
         HStack {
