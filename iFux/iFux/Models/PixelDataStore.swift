7bfa7e027b3e019978c051f009f7daaf551fc5c9
Simon Morgenstern <--global>
2022-04-19 21:49:35 +0200

    feat: add PixelDataStore
    wtf is going on with Xcode git integration :/

---
+++ iFux/iFux/Models/PixelDataStore.swift
@@ -0,0 +1,38 @@
+//
+//  PixelDataStore.swift
+//  iFux
+//
+//  Created by Simon Morgenstern on 19.04.22.
+//
+
+import Foundation
+import UIKit
+
+final class PixelDataStore: ObservableObject {
+    @Published var pixelData: [PixelData]?
+    var maxPixelX: Double = 0
+    var maxPixelY: Double = 0
+
+    init() {
+        loadData()
+    }
+
+    func loadData() {
+        if let url = Bundle.main.url(forResource: "coords", withExtension: "json") {
+            do {
+                let jsonData = try! Data(contentsOf: url)
+                pixelData = try! JSONDecoder().decode([PixelData].self, from: jsonData)
+                if let data = pixelData {
+                    for index in 0..<data.count {
+                        if data[index].x > maxPixelX {
+                            maxPixelX = data[index].x
+                        }
+                        if data[index].y > maxPixelY {
+                            maxPixelY = data[index].y
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
